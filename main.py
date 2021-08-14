531c25564195c7f02c0ed0aea9ee11fdd316d084
SuperSonic <supersonic@livemail.tw>
2021-08-15 01:23:26 +0800

    Replace requests with aiohttp

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
+import asyncio
 import json
 import os
-import time
 from base64 import b64decode
 from urllib.parse import urlencode
 
-import requests
+import aiohttp
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_point = os.getenv("POP_API_POINT")
-next_token = ""
 
 
-def pop() -> None:
-    global next_token
+async def main() -> None:
+    tokens = ["" for _ in range(20)]
+    while True:
+        async with aiohttp.ClientSession() as session:
+            promises = [pop(session, token) for token in tokens]
+            tokens = await asyncio.gather(*promises)
+
+
+async def pop(session: aiohttp.ClientSession, token: str) -> str:
+    url = get_request_url(token)
+    async with session.post(url) as response:
+        if response.status in [200, 201]:
+            result = await response.json()
+            next_token = result["new_token"]
+            data = next_token.split(".")
+            if len(data) == 3:
+                missing_padding = 4 - len(data[1]) % 4
+                if missing_padding:
+                    data[1] += "=" * missing_padding
+                token_raw = b64decode(data[1])
+                print(json.dumps(json.loads(token_raw), sort_keys=True, indent=4))
+            return next_token
+        else:
+            print(f"Error: {response.status}")
+    return ""
+
+
+def get_request_url(token: str) -> str:
     query = {
         'count': 800,
-        'token': next_token,
+        'token': token,
         'captcha_token': ''
     }
-    url = f"{api_point}?{urlencode(query)}"
-    response = requests.post(url)
-    if response.status_code in [200, 201]:
-        result = response.json()
-        next_token = result["new_token"]
-        data = next_token.split(".")
-        if len(data) == 3:
-            missing_padding = 4 - len(data[1]) % 4
-            if missing_padding:
-                data[1] += "=" * missing_padding
-            token_raw = b64decode(data[1])
-            print(json.dumps(json.loads(token_raw), sort_keys=True, indent=4))
-    else:
-        print(f"Error: {response.status_code}")
+    return f"{api_point}?{urlencode(query)}"
 
 
 if __name__ == '__main__':
     while True:
-        pop()
-        time.sleep(1)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())

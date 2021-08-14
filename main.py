6af9286caa056df934c51806f2b8c10682978e86
SuperSonic <supersonic@livemail.tw>
2021-08-14 17:15:49 +0800

    Update

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 import time
 from base64 import b64decode
 from urllib.parse import urlencode
-from dotenv import load_dotenv
 
 import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -22,15 +22,18 @@ def pop() -> None:
     }
     url = f"{api_point}?{urlencode(query)}"
     response = requests.post(url)
-    result = response.json()
-    next_token = result["new_token"]
-    data = next_token.split(".")
-    if len(data) == 3:
-        missing_padding = 4 - len(data[1]) % 4
-        if missing_padding:
-            data[1] += "=" * missing_padding
-        token_raw = b64decode(data[1])
-        print(json.dumps(json.loads(token_raw), sort_keys=True, indent=4))
+    if response.status_code in [200, 201]:
+        result = response.json()
+        next_token = result["new_token"]
+        data = next_token.split(".")
+        if len(data) == 3:
+            missing_padding = 4 - len(data[1]) % 4
+            if missing_padding:
+                data[1] += "=" * missing_padding
+            token_raw = b64decode(data[1])
+            print(json.dumps(json.loads(token_raw), sort_keys=True, indent=4))
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == '__main__':
